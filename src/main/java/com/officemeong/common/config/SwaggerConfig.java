3d91eda865a3ee7208d0318fa61d76ca338a9e48
dietken1 <dietken1@ajou.ac.kr>
2026-08-09 17:46:18 +0900

    fix: Swagger 서버 URL 하드코딩 제거하여 dev 환경 Try it out 실패 수정
    servers에 localhost:8080/api.officemeong.com만 고정돼 있어 dev(EC2 IP)
    Swagger UI에서 Execute 시 엉뚱한 호스트로 요청이 나가 "Failed to fetch"가
    발생했음. .servers() 제거 시 springdoc이 실제 요청 Host 기준으로 서버
    URL을 자동 생성하므로, 로컬/dev/운영 어디서 열어도 맞는 호스트로 잡힘.

--- src/main/java/com/officemeong/common/config/SwaggerConfig.java
+++ src/main/java/com/officemeong/common/config/SwaggerConfig.java
@@ -6,11 +6,8 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -53,10 +50,6 @@ public class SwaggerConfig {
                         .contact(new Contact()
                                 .name("OfficeMeong 개발팀")
                                 .email("a01057571492@gmail.com")))
-                .servers(List.of(
-                        new Server().url("http://localhost:8080").description("로컬 개발 서버"),
-                        new Server().url("https://api.officemeong.com").description("운영 서버")
-                ))
                 .components(new Components()
                         .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
